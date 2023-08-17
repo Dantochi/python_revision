8c009835ff69edd78cdc7e1db1ffa6c15a6c9f64
Tochukwu Chidi <chiditochukwudaniel@gmail.com>
2023-08-17 13:44:54 +0100

    My first commit

---
+++ revision.py
@@ -0,0 +1,9 @@
+# print(5.574849/9.03404)
+# thislist = ["apple", "banana", "carrot"]
+# print(type(thislist))
+
+# thislist = list(("apple", "banana", "cherry"))
+# print(thislist)
+
+mytuple = ("apple", "banana", "cherry", "delaware", "egg")
+print(mytuple)
